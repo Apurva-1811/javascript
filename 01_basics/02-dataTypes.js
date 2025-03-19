b251618e15564cbd021cb53fc1cc155333eedf3c
Apurva-1811 <145369949+Apurva-1811@users.noreply.github.com>
2025-03-19 12:46:25 +0000

    data types and variables

---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code and newer version
+
+//alert(3+3) // we are using nodejs not browser 
+
+let name = "APurva"
+let age = 21 
+
+//number =>2 to pwer 53
+//bigint 
+//string=> ""
+//boolean => true/false 
+// null => stand alone value 
+//undefined => not assigned value 
+//symbol => to define uniqueness in react and figma 
+
+
+//object 
+
+
+console.log(typeof null  );//object //maybe beacuse null is an object
+console.log(typeof Boolean)//fuvtion//any primitive data type will of tpe function
+console.log(typeof undefined);//undefined
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accId=144553
+let accemail= "apu@gmail.com"
+var accPass= "12345"
+accCity = "jaipur" 
+
+//accId = 2 //not allowed 
+ accemail= "tapu@gmail.com"
+var accPass = "23451"//preffer not to use because of block and functional scope 
+accCity= "Hisar"
+
+console.log(accId);
+console.table([accId , accemail , accPass, accCity])
+
