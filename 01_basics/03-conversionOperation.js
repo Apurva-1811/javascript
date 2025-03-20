e2a0fe9ead1a21588b1e03ce9b632bab3edd8ce3
Apurva-1811 <145369949+Apurva-1811@users.noreply.github.com>
2025-03-20 11:56:08 +0000

    discussion of all conversions

--- 01_basics/03-conversionOperation.js
+++ 01_basics/03-conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null// 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let value = Number(score);
-console.log(typeof value);
-console.log(value); // Nan i.e not a number // drwaback convert to ho jaega but number nhi bnega 
+// console.log(typeof value);
+// console.log(value); // Nan i.e not a number // drwaback convert to ho jaega but number nhi bnega 
 
 //"33" => 33
 //"33abc" => NaN
@@ -15,18 +15,44 @@ console.log(value); // Nan i.e not a number // drwaback convert to ho jaega but 
 let isLoggedIn = "";
 
 let booleanIs = Boolean(isLoggedIn)
-console.log(booleanIs);
+// console.log(booleanIs);
 
 //1 => true ; 0 => false
 //"" => false
 //"histesh" => true
 
 let a = 9
-console.log( typeof a);
+// console.log( typeof a);
 
 let StringNum = String(a);
-console.log(StringNum);
-console.log(typeof StringNum);
+// console.log(StringNum);
+// console.log(typeof StringNum);
+
+
+//************ Operations*************** */
+
+let values = 3 ; 
+let negvalue = -values
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " Apurva"
+// console.log(str1+str2);
+
+// console.log("1" + 2); //12
+// console.log(1+ "2"); //12
+// console.log("1" + 2 +2);//122
+// console.log(1 + 2 +"2");//32
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2+4
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter);
+
+
+
 
 
 
