60f8596d0611c9ffe4f214b5dc7359e809b581df
Apurva-1811 <145369949+Apurva-1811@users.noreply.github.com>
2025-03-20 12:11:17 +0000

    important comparisions

---
+++ 01_basics/04-comparision.js
@@ -0,0 +1,15 @@
+console.log(2 <"1"); 
+
+console.log(false<"1") //true
+console.log( false <"") // false
+console.log(false == "");//true
+
+//avaoid thes kind of consversions 
+console.log(null>0)//false
+console.log(null==0);//false
+console.log( null >= 0);//true
+
+//=== strict check that is also checks data types 
+
+console.log("2" === 2)//false 
+console.log("2" == 2)//true 
