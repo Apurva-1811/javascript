8a1fcb7aafb752f82c7d915572d629e7a890547b
Apurva-1811 <145369949+Apurva-1811@users.noreply.github.com>
2025-03-20 11:33:51 +0000

    discussion of data type conversion

---
+++ 01_basics/03-conversionOperation.js
@@ -0,0 +1,34 @@
+let score = null// 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let value = Number(score);
+console.log(typeof value);
+console.log(value); // Nan i.e not a number // drwaback convert to ho jaega but number nhi bnega 
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1
+//null =>0 
+// undefined =>Nan
+let isLoggedIn = "";
+
+let booleanIs = Boolean(isLoggedIn)
+console.log(booleanIs);
+
+//1 => true ; 0 => false
+//"" => false
+//"histesh" => true
+
+let a = 9
+console.log( typeof a);
+
+let StringNum = String(a);
+console.log(StringNum);
+console.log(typeof StringNum);
+
+
+
+ 
+
