2653c777ecbf95f3179b4b3d6140340ac943f389
Apurva-1811 <145369949+Apurva-1811@users.noreply.github.com>
2025-03-21 11:30:25 +0000

    string and basic methods

---
+++ 01_basics/05-Strings.js
@@ -0,0 +1,23 @@
+const name = "Apurva"
+
+const repocount = 5 ;
+
+//console.log(name +repocount)
+
+//console.log(`hello my name is ${name} and my repocount is ${repocount}`)
+
+//declaring string
+const game = new String('a-apurva')//now this string is an object 
+// console.log(game[0]);
+
+// console.log(game.length);
+// console.log(game.toUpperCase());
+// console.log(game.substring(0,4));
+console.log(game.slice(-8,4));
+
+
+
+
+
+
+
