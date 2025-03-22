18ea159e1b186ec07eee288685cf16a0fc277ae0
Apurva-1811 <145369949+Apurva-1811@users.noreply.github.com>
2025-03-22 12:08:42 +0000

    basics of array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+ //array
+ const arr = [0,1,2,3,4,5 , true , "APurva"];
+// console.log(arr[7]);
+
+//another method to declare array 
+const nums = new Array(1,2,3,4,4)
+// console.log(nums[3]);
+
+
+//array methods /
+arr.push(6)
+arr.pop()
+
+arr.unshift(9);// push to start of array 
+arr.shift();//negation of unshift 
+
+// console.log(arr.includes(9));
+// arr.join(); // converts array to string
+/////////doubt 
+
+//console.log(arr);
+
+//slice and splice 
+
+//console.log(arr)
+
+const newarr = arr.slice(1,3)
+console.log(newarr);
+console.log(arr);
+const newarr2 = arr.splice(1,3)
+console.log(newarr2);
+console.log(arr);
+
+
+
+
+
